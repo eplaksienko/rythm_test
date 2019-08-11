36ae317666bede038b3b8e58778fa76d87dd796c
eplaksienko <eplaksienko@hotmail.com>
2019-08-11 11:46:40 -0400

    add comments and readme

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# rythm_test
+# rythm_test user manual
+
+# Prepare environment
+
+ - download and install [Python] (https://www.python.org/downloads/) version 3.5
+ - setup virtual invironment, activate it and install pytest in virtual envoronment, use cmd for example for it in windows
+
+ ```sh
+ $ cd [project directory]
+ $ python -m venv .venv
+ $ .venv\Scripts\activate.bat
+ $ pip install pip install vendor\pytest-5.0.1-py3-none-any.whl
+ ```
+
+# Execute the project
+
+ - Execute actual project
+
+ ```sh
+ $python main.py
+ ```
+
+ - Execute unit tests
+
+ ```sh
+  $pytest tests\unit
+  ```
+
+  That's it folks
--- word_counter/word_counter.py
+++ word_counter/word_counter.py
@@ -3,20 +3,26 @@ import re
 class WordCounter:
     @staticmethod
     def count_words(sentence):
+        #check if word is not empty (0 len) and input is not None
         if sentence:
+            #use regular expression in order to get words only (commas, periods and etc are not part of the words)
             words = re.findall(r"[\w']+", sentence)
+            #prepare structure (dictionary)
             current_return = {
                 "words": "",
                 "length": 0,
             }
+            #iterate through the list of the words
             for word in words:
+                #if find longer add to structure
                 if current_return.get("length") < len(word):
                     current_return.update({
                         "words": [word],
                         "length": len(word),
                     })
+                #if we have the word as long as the current longest just add it to the list of the words
                 elif current_return.get("length") == len(word):
                     current_return.get("words").append(word)
-
+            #return structure
             return current_return
 
